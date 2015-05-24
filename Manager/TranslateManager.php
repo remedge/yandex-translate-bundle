484351c88c56afde69022360303f5b6b3c8e226c
Alexander <sashaaro@gmail.com>
2015-05-24 18:54:58 +0300

    Update TranslateManager.php

--- Manager/TranslateManager.php
+++ Manager/TranslateManager.php
@@ -8,18 +8,18 @@ use Yandex\Translate\Exception;
 class TranslateManager
 {
     private $apiKey;
-
+    private $translator;
+    
     public function __construct($apiKey)
     {
         $this->apiKey = $apiKey;
+        $this->translator = new Translator($this->apiKey);
     }
 
     public function translate($string, $translateDirection)
     {
         try {
-            $translator = new Translator($this->apiKey);
-
-            $translation = $translator->translate($string, $translateDirection);
+            $translation = $this->translator->translate($string, $translateDirection);
 
             return $translation;
         } catch (Exception $e) {
